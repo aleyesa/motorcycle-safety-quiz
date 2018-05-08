44c6fec1abd54c5da40ccc86154ab1b67a3fb09d
Aaron Leyesa <aaron.leyesa@gmail.com>
2018-05-07 20:00:04 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// 'use strict';
 //counters used to get next question, update scoreCounter, and used to check how many questions done.
 let questionIndexCounter = 0;
 let scoreCounter = 0;
@@ -9,28 +8,11 @@ let userCorrect;
 const questionsAnswers = [
   {
     //Question 1
-    question: 'Q1: What are the four preparations you should do when preparing to ride a motorcycle?',
-    a1: [`a) 
-    1. You fit the motorcycle.
-    2. You inspect the motorcycle.
-    3. You use proper protective riding gear.
-    4. You have a good mental attitude with safety as your priority.`, {correct: 'correct'}],
-    a2: [`b)
-    1. You look cool on a motorcycle.
-    2. You inspect the motorcycle.
-    3. You use proper protective riding gear.
-    4. You have a good mental attitude with safety as your priority.`, {correct: 'wrong'}],
-    a3: [`c)
-    1. You fit the motorcycle.
-    2. You think that your motorcycle was good yesterday,
-    so you should be fine today without basic inspection.
-    3. You use proper protective riding gear.
-    4. You have a good mental attitude with safety as your priority.`, {correct: 'wrong'}],
-    a4: [`d)
-    1. You fit the motorcycle.
-    2. You inspect the motorcycle.
-    3. You use proper protective riding gear.
-    4. If you have no fear go right ahead.`, {correct: 'wrong'}],
+    question: 'Q1: What is not a reccommended preparation you should do when preparing to ride a motorcycle?',
+    a1: [`a) You fit the motorcycle.`, {correct: 'wrong'}],
+    a2: [`b) Having a good mental attitude with safety as your priority.`, {correct: 'wrong'}],
+    a3: [`c) There is no need for basic inspection everytime you ride.`, {correct: 'correct'}],
+    a4: [`d) You use proper protective riding gear.`, {correct: 'wrong'}],
   },
   //Question 2
   {
@@ -51,10 +33,10 @@ const questionsAnswers = [
   //Question 4
   {
     question: 'Q4: A procedure called FINE-C is used as an engine pre-start routine, what does it stand for?',
-    a1: ['a) Focus, Ignition, Neutral, Engine Cut-off Switch, Choke/Clutch', {correct: 'correct'}],
-    a2: ['b) Fuel, Ignition, Neutral, Engine, Cut-off Switch, Choke/Clutch', {correct: 'wrong'}],
+    a1: ['a) Focus, Ignition, Neutral, Engine Cut-off Switch, Choke/Clutch', {correct: 'wrong'}],
+    a2: ['b) Fuel, Ignition, Neutral, Engine, Cut-off Switch, Choke/Clutch', {correct: 'correct'}],
     a3: ['c) Fuel, Intuition, Neutral,  Engine, Cut-off Switch, Choke/Clutch', {correct: 'wrong'}],
-    a4: ['d) Fuel, Ignition, Normal, Enginge, Cut-off Switch, Choke/Clutch', {correct: 'wrong'}]
+    a4: ['d) Fuel, Ignition, Normal, Engine, Cut-off Switch, Choke/Clutch', {correct: 'wrong'}]
   },
   //Question 5
   {
@@ -76,17 +58,17 @@ const questionsAnswers = [
   {
     question: 'Q7: What are the proper protective gear you should wear?',
     a1: ['a) Helmet, footwear, pants, jacket, and gloves.', {correct: 'correct'}],
-    a2: ['b) anything that makes you comfortable.', {correct: 'wrong'}],
+    a2: ['b) Anything that makes you comfortable.', {correct: 'wrong'}],
     a3: ['c) Just a helmet and gloves should be fine.', {correct: 'wrong'}],
     a4: ['d) Just a helmet.', {correct: 'wrong'}]
   },
   //Question 8
   {
     question: 'Q8: Where do crashes occur most often?',
-    a1: ['a) highways', {correct: 'wrong'}],
-    a2: ['b) curves', {correct: 'wrong'}],
-    a3: ['c) intersections', {correct: 'wrong'}],
-    a4: ['d) both b and c.', {correct: 'correct'}]
+    a1: ['a) Highways', {correct: 'wrong'}],
+    a2: ['b) Curves', {correct: 'wrong'}],
+    a3: ['c) Intersections', {correct: 'wrong'}],
+    a4: ['d) Both b and c.', {correct: 'correct'}]
   },
   //Question 9
   {
@@ -106,31 +88,33 @@ const questionsAnswers = [
   }
   ];
 
-//function that adds a form to quizSection using questionsAnswers data.
+//function that creates a form to display question and its available answers using 'questionAnswers' data.
 const showQuestion = function(qIndex){
-  unhideElement('.quizSection'); //hide quiz section
+  unhideElement('.quizSection'); //show quiz section
   $('.quizSection').append(`
-  <form id="quiz" action="/Motorcycle-Safety-Quiz" method="get">
-    <h2>${questionsAnswers[qIndex].question}</h2>
-    <div>
-      <input role="radio" type="radio" id="answer1" class="js-answer" name="answers" value="${questionsAnswers[qIndex].a1[1].correct}" required/>
-      <label for="answer1">${questionsAnswers[qIndex].a1[0]}</label>
-    </div>
-    <div>
-      <input role="radio" type="radio" id="answer2" class="js-answer" name="answers" value="${questionsAnswers[qIndex].a2[1].correct}"/>
-      <label for="answer2">${questionsAnswers[qIndex].a2[0]}</label>
-    </div>
-    <div>
-      <input role="radio" type="radio" id="answer3" class="js-answer" name="answers" value="${questionsAnswers[qIndex].a3[1].correct}"/>
-      <label for="answer3">${questionsAnswers[qIndex].a3[0]}</label>
-    </div>
-    <div>
-      <input role="radio" type="radio" id="answer4" class="js-answer" name="answers" value="${questionsAnswers[qIndex].a4[1].correct}"/>
-      <label for="answer4">${questionsAnswers[qIndex].a4[0]}</label>
-    </div>
-    <div class="center">
-      <input role="button" class="checkAnswerBtn" type="submit" value="Check Answer" />
-    </div>
+  <form class="quiz-form" action="/Motorcycle-Safety-Quiz" method="get">
+    <fieldset name="qA">
+      <legend class="question">${questionsAnswers[qIndex].question}</legend>
+      <section role="region" class="answerSection">
+        <input role="radio" type="radio" id="answer1" class="js-answer" name="answers" value="${questionsAnswers[qIndex].a1[1].correct}" required autofocus/>
+        <label for="answer1">${questionsAnswers[qIndex].a1[0]}</label>
+      </section>
+      <section role="region" class="answerSection">
+        <input role="radio" type="radio" id="answer2" class="js-answer" name="answers" value="${questionsAnswers[qIndex].a2[1].correct}"/>
+        <label for="answer2">${questionsAnswers[qIndex].a2[0]}</label>
+      </section>
+      <section role="region" class="answerSection">
+        <input role="radio" type="radio" id="answer3" class="js-answer" name="answers" value="${questionsAnswers[qIndex].a3[1].correct}"/>
+        <label for="answer3">${questionsAnswers[qIndex].a3[0]}</label>
+      </section>
+      <section role="region" class="answerSection">
+        <input role="radio" type="radio" id="answer4" class="js-answer" name="answers" value="${questionsAnswers[qIndex].a4[1].correct}"/>
+        <label for="answer4">${questionsAnswers[qIndex].a4[0]}</label>
+      </section>
+      <section role="region" class="answerBtnSection">
+        <input role="button" class="checkAnswerBtn" type="submit" value="Check Answer"/>
+      </section>
+    </fieldset>
   </form>
 `);
 };
@@ -145,23 +129,20 @@ const unhideElement = function(selector, dispValue = 'block'){
     $(selector).css('display', dispValue);
 };
 
-//Displays a quote bubble of encouragement
-const showEncouragement = function(phrase){
+//Displays a quote bubble of encouragement for a limited time
+const showEncouragement = function(phrase, timeIn = 400){
     $('.quoteBubble p').text(phrase);
     unhideElement('.quoteBubble', 'inline');
+    $('.quoteBubble').fadeIn(timeIn);
   };
 
-//hides all elements not related to start screen content and a button used to 
-//navigate to the next section.
+//Show start screen content, when user clicks on begin button, we show the score and quiz form, and a word of
+//encouragement!
 const startQuiz = function() {
-  //hides all sections other than start screen content
-  hideElement('.scoringSection');
-  hideElement('.quizSection');
-  hideElement('.feedbackSection');
-  hideElement('.finalSection');
   //Start questions by using a click event on begin button
   $('.beginBtn').on('click', function(event) {
     showEncouragement('Good luck!');
+    $('.quoteBubble').fadeOut(1500);
     hideElement('.startSection'); 
     unhideElement('.scoringSection'); //displays default score section
     //show first question
@@ -173,7 +154,6 @@ const startQuiz = function() {
 //check if user got correct answer for questions
 const checkAnswer = function() {
   $('.quizSection').on('click', '.js-answer', function(event) {
-    hideElement('.quoteBubble');
     hideElement('.feedbackSection');
     $('.usersAnswer').text(`You got it ${this.value}!`);
     userCorrect = this.value; //changes userCorrect value to use for updating score.
@@ -184,6 +164,7 @@ const checkAnswer = function() {
 const getFeedback = function() {
 $('.quizSection').on('submit', function(event) {
   event.preventDefault(); //prevents default form submission to server.
+  hideElement('.quizSection');
   unhideElement('.feedbackSection');
   //iterate all elements that has class 'js-answer' to find the correct answer and modify the inner text
   // of all elements with the class 'correctAnswer'.
@@ -192,20 +173,27 @@ $('.quizSection').on('submit', function(event) {
       $('.correctAnswer').text('The correct answer is ' +  $(this).siblings('label').text());
     }
   });
+  //Gives words of encouragement based off of whether user got correct answer or not
+  if(userCorrect === 'correct'){
+    showEncouragement('Keep it up!');
+  }else {
+    showEncouragement('No worries!');
+  }
 });
 };
 
 //adds specific text to final section based on users total correct answers
 const updateFinalSection = function() {
   if(scoreCounter === questionsAnswers.length) {
-    $('.finalSection').append('<p>You know your stuff!</p>');
+    $('.finalSection').append('<p class="impression">You know your stuff!</p>');
   }else if(scoreCounter > 6){
-    $('.finalSection').append('<p>Pretty good!</p>');
+    $('.finalSection').append('<p class="impression">Pretty good!</p>');
   }else {
-    $('.finalSection').append('<p>Nice try, might want to review a little more.</p>');
+    $('.finalSection').append('<p class="impression">Nice try, might want to review a little more.</p>');
   }
-  $('.finalSection').append('<img role="image" src="http://www.mysafetysign.com/img/lg/S/Be-Alert-Safety-Slogan-Sign-S-4107.gif" alt="Motorycle Slogan: Be Alert, Expect The Unexpected"/>');
-  $('.finalSection').append('<button role="button" class="finalBtn">Go back to start screen</button>');
+  $('.finalSection').append(`
+      <img role="image" src="http://www.mysafetysign.com/img/lg/S/Be-Alert-Safety-Slogan-Sign-S-4107.gif" alt="Motorycle Slogan: Be Alert, Expect The Unexpected"/>
+      <button role="button" class="finalBtn">Go back to start screen</button>`);
 };
 
 //resets and goes to start section
@@ -228,13 +216,11 @@ const proceedAndUpdate = function() {
   $('.proceedBtn').on('click', function(event) {
       //checks if user chose correct answer, if true increment score by 1.
       if(userCorrect === 'correct'){
-        showEncouragement('Keep it up!'); 
         scoreCounter++;
-      }else {
-        showEncouragement('No worries!');
       }
       //hides feedback section
       hideElement('.feedbackSection');
+      hideElement('.quoteBubble');
       //update score
       $('.score').text(`Score: ${scoreCounter} / ${questionsAnswers.length}`);
       //clears  everything within quizSection
@@ -245,7 +231,6 @@ const proceedAndUpdate = function() {
       //else display next question.
       if(questionIndexCounter === questionsAnswers.length){
         hideElement('.quizSection');
-        // hideElement('.feedbackSection');
         unhideElement('.finalSection');
         showEncouragement('Ride safe!');
         updateFinalSection();
@@ -271,8 +256,8 @@ $(updateQuiz());
 //Future ideas:
 //randomize questionsAnswers
 //animations
-//universal function for adding questions Answers
-
-
+//universal function for adding questions and answers
+//admin screen
+//implement a css grid style responsive design
 
 
